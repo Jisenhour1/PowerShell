ad82d8f68af9d21c9d3903e7df4d404e5d2a1aff
jisenhour <jisenhour1@gmail.com>
2017-07-10 11:33:18 -0400

    Finished First correct version

--- PowerShellGeneral/copy-Directory.ps1
+++ PowerShellGeneral/copy-Directory.ps1
@@ -21,21 +21,78 @@
 function Copy-Directory (OptionalParameters) 
 {
     Begin
+    param(
+        [Parameter(Mandatory=$True,
+            HelpMessage="The full path to source directory")]
+        [Alias('source')]
+        [string] $SourceDirectory,
+
+        [Parameter(Mandatory=$True,
+                   ValueFromPipeline=$True,
+                   ValueFromPipelineByPropertyName=$True,
+                   HelpMessage="The full path to the base directory")]
+        [Alias('Target')]
+        [string[]]$TargetDirectory,
+
+        [Parameter(Mandatory=$False,
+                HelpMessage="File name. or pattern to exlude from copy operation")]
+        [string]$Exlude,
+
+        [Parameter()]
+        [string]$ErrorLogFilePath = $Logfile
+    )
+    BEGIN 
+    {
+        $ErrorsHappened = $False
+        Remove-Item -Path $ErrorLogFilePath -Force -ErrorAction SilentlyContinue
+        if (!(Test-Path $SourceDirectory ))
         {
-        }
-        Process
+            Write-Verbose "$SourceDirectory not found"
+            "Source $SourceDirectory not found"| Out-File $ErrorLogFilePath -Append
+            $ErrorsHappened = $True
+        }  
+        if($ErrorsHappened) 
         {
-            if ($pscmdlet.ShouldProcess("Target", "Operation"))
+            Break
+        }    
+        $ErrorsHappened = $False
+    }
+    PROCESS {
+        Write-Verbose "HERE WE GO!!!!"
+        foreach ($Directory in $TargetDirectory)
+        {
+            try 
+            {   
+
+                Write-Verbose "------------------------------"
+                Write-Verbose "Starting to cop to $TargetDirectory"
+                if ((Test-Path $TargetDirectory ))
+                {
+                    $DirList = Get-ChildItem $SourceDirectory -Recurse -Exclude $exclude 
+                    $DirList| Copy-Item -Destination {Join-Path $TargetDirectory $_.FullName.Substring($SourceDirectory.length)}    
+                }   
+                Else
+                {
+                     Write-Verbose "$TargetDirectory not found"
+                    "Target $TargetDirectory not found"| Out-File $ErrorLogFilePath -Append
+                    $ErrorsHappened = $True
+                }
+                
+                
+
+            } 
+            catch 
             {
-                $source = 'd:\t1'
-                $dest = 'd:\t2'
-                $exclude = @('web.config')
-                $DirList = Get-ChildItem $source -Recurse -Exclude $exclude 
-                $DirList| Copy-Item -Destination {Join-Path $dest $_.FullName.Substring($source.length)}
-            }
+
+                Write-Verbose "Couldn't vopy to $TargetDirectory"
+                "Failed to copy to $TargetDirectory" | Out-File $ErrorLogFilePath -Append
+                $ErrorsHappened = $True
+            } 
         }
-        End
-        {
+    }
+    END {
+        if ($ErrorsHappened) {
+            Write-Warning "OMG, errors. Logged to $ErrorLogFilePath."
         }
-    }  
+    }
 }
